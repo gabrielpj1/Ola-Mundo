c811784a95314f6343d460c9e55a8d4314a25092
Gabriel Alves <gabriel_pj1@hotmail.com>
2023-11-26 20:03:48 +0000

    Adicionei uma linha separatória

--- teste.py/praticando.py
+++ teste.py/praticando.py
@@ -2,3 +2,4 @@ print('Olá, Mundo!')
 
 usuario = str(input('Digite seu nome: '))
 print(f'Olá, {usuario}.\nSeja muito bem vindo(a) ao meu repositório de testes \033[4;31mPython\033[m.')
+print('-=' * 30)
